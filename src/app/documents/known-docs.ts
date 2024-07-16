bbe0f44082887bd5bc0a8732d1d265489cd05577
Tomasz Zielinski <tomasz.zielinski@ed.ac.uk>
2024-07-16 19:30:25 +0100

    improved T&C and embargo description

--- src/app/documents/known-docs.ts
+++ src/app/documents/known-docs.ts
@@ -2,12 +2,14 @@ export const StaticDocsOptions = [
 
   ['about', 'About', 'Documentation'],
   ['service', 'Service Description (T&C)', 'Service Description (T&C)'],
+  ['embargo','Embargo period explained','Embargo period explained'],
   ['privacy', 'Privacy Statement', 'Privacy Statement'],
   ['faq', 'Frequently Asked Questions (FAQ)', 'FAQ'],
   ['timeseries-data', 'TimeSeries data and formats', 'Timeseries data'],
   ['period-methods', 'Methods of period analysis', 'Period analysis methods'],
   ['detrending', 'Detrending for period analysis', 'Detrending and analysis'],
   ['phases', 'Phase calculation', 'Phase calculation'],
+  ['service-2017-01-20', 'Service Description 2017', 'Service Description 2017'],
 ];
 
 export function isKnownStaticDoc(name: string) {
