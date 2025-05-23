a84da6aedf3ca3dd9a7da786359e908c1132e367
DanielThedie <dthedie@gmail.com>
2025-05-23 11:31:11 +0100

    Update dataset release message

--- src/app/page/welcome/welcome.component.ts
+++ src/app/page/welcome/welcome.component.ts
@@ -50,7 +50,7 @@ export class WelcomeComponent implements OnInit, OnDestroy {
 
   showMaintenance(): boolean {
     console.log("",new Date());
-    return ((new Date()) < (new Date(2025, 4, 11)));
+    return ((new Date()) < (new Date(2025, 3, 11))); // Note date uses month *index* (0-11)
   }
 
 }
