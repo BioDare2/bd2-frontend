4fa929cdccb09845877e902aefcb6ae8f3e7057f
DanielThedie <dthedie@gmail.com>
2025-06-17 13:31:53 +0100

    Update bad credentials message

--- src/app/backend/biodare-rest.service.ts
+++ src/app/backend/biodare-rest.service.ts
@@ -666,7 +666,9 @@ export class BioDareRestService {
 
     switch (resp.status) {
       case 401: {
-        message = 'Bad credentials, locked or not activated account';
+        message = 'Could not connect (bad credentials, account locked or non-activated)\n' +
+        'If you have just registered, please check your email for the activation link.\n' +
+        'If you have entered several incorrect passwords, your account may be locked for the next 30 min.';
         break;
       }
       case 403: {
