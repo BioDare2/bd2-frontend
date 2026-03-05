2f1ab9774564f6038f73f49cc94bf9af50eec49d
DanielThedie <dthedie@gmail.com>
2026-03-05 10:06:36 +0000

    Update Angular core and cli to 21

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import { provideZoneChangeDetection } from "@angular/core";
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
@@ -10,5 +11,5 @@ if (environment.production) {
 }
 */
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule, { applicationProviders: [provideZoneChangeDetection()], })
   .catch(err => console.error(err));
